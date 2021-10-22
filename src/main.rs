63dddc77010c284f308a3a0ab30927253a1790e3
Miles Glapa-Grossklag <miles@glapa-grossklag.com>
2021-10-22 09:05:39 -0700

    Read from STDIN by default

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,21 @@
-#![allow(unused_imports)]
-#![allow(unused_variables)]
-#![allow(unused_mut)]
 use std::fs::File;
-use std::io::{self, Write, BufRead, BufReader};
-
+use std::io::{self, BufRead, BufReader, Write};
 
 fn main() {
-    let use_stdin: String = "-".to_owned();
+    let use_stdin = "-";
 
+    // Process args.
+    let mut args: Vec<_> = std::env::args().skip(1).collect();
+    if args.is_empty() {
+        args.push(use_stdin.into());
+    }
+
+    // Read and write.
     let mut contents: Vec<u8> = Vec::new();
-
-    for arg in std::env::args().skip(1) {
+    for arg in args {
         contents.clear();
 
-        let mut file: Box<dyn BufRead> = if arg == "-" {
+        let mut file: Box<dyn BufRead> = if arg == use_stdin {
             Box::new(BufReader::new(io::stdin()))
         } else {
             Box::new(BufReader::new(File::open(&arg).unwrap()))
@@ -25,8 +27,7 @@ fn main() {
 
         let mut stdout = io::stdout();
         if stdout.write(&contents).is_err() {
-            panic!("couldn't write to: {}", arg);
+            panic!("couldn't write to stdout");
         }
-
     }
 }
