d4378a2169f5f1fa60d77898a4bf413b8cfe45eb
Miles Glapa-Grossklag <miles@glapa-grossklag.com>
2021-10-22 09:02:07 -0700

    Read from STDIN when directed

--- src/main.rs
+++ src/main.rs
@@ -2,23 +2,28 @@
 #![allow(unused_variables)]
 #![allow(unused_mut)]
 use std::fs::File;
-use std::io::{self, Read, Write};
+use std::io::{self, Write, BufRead, BufReader};
+
 
 fn main() {
-    let mut stdin = io::stdin();
-    let mut stdout = io::stdout();
+    let use_stdin: String = "-".to_owned();
+
+    let mut contents: Vec<u8> = Vec::new();
 
     for arg in std::env::args().skip(1) {
-        let mut file = match File::open(&arg) {
-            Err(why) => panic!("couldn't open {}: {}", arg, why),
-            Ok(file) => file,
+        contents.clear();
+
+        let mut file: Box<dyn BufRead> = if arg == "-" {
+            Box::new(BufReader::new(io::stdin()))
+        } else {
+            Box::new(BufReader::new(File::open(&arg).unwrap()))
         };
 
-        let mut contents: Vec<u8> = Vec::new();
         if file.read_to_end(&mut contents).is_err() {
             panic!("couldn't read from: {}", arg);
         }
 
+        let mut stdout = io::stdout();
         if stdout.write(&contents).is_err() {
             panic!("couldn't write to: {}", arg);
         }
